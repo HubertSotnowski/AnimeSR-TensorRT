dc29fe6fc818f2d526e07a1c3a19e3d4f61697d7
Hubert Sontowski <72866544+HubertSotnowski@users.noreply.github.com>
2022-11-28 22:30:28 +0100

    fixed dynamic shapes, improved script, removed unused files

--- convert.py
+++ convert.py
@@ -1,26 +1,37 @@
-
+import argparse
 import torch
+import os
+from vsr_arch import MSRSWVSR
 
-from animesr.archs.vsr_arch import MSRSWVSR
+my_parser = argparse.ArgumentParser(description=" ")
+my_parser.add_argument("--input", metavar="--input", type=str, help="input model")
+my_parser.add_argument("--output", metavar="--output", type=str, help="output model")
+my_parser.add_argument("--height", metavar="--height", type=int, help="height")
+my_parser.add_argument("--width", metavar="--width", type=int, help="width")
+args = my_parser.parse_args()
+
 
 model = MSRSWVSR(num_feat=64, num_block=[5, 3, 2], netscale=4)
 
-print(model)
 
-model.load_state_dict(torch.load("D:\\repo\\AnimeSR\\weights\\AnimeSR_v2.pth"),strict=False)
-
+model.load_state_dict(torch.load(args.input), strict=False)
 input_names = ["input"]
 output_names = ["output"]
-f1=torch.rand((1,6,540,640))
-x=(f1)
+f1 = torch.rand((1, 6, args.height, args.width))
+x = f1
 
-torch.onnx.export(model,               # model being run
-                  x,                         # model input (or a tuple for multiple inputs)
-                  "weights/AnimeSR_v2.pth",   # where to save the model (can be a file or file-like object)
-                  export_params=True,        # store the trained parameter weights inside the model file
-                  opset_version=16,          # the ONNX version to export the model to
-                  do_constant_folding=True,  # whether to execute constant folding for optimization
-                  input_names = input_names,   # the model's input names
-                  output_names = output_names) #                  dynamic_axes={'input' : {3 : 'width', 2: 'height'}})
-
-print('Successfully converted model to onnx')
+torch.onnx.export(
+    model,  # model being run
+    x,  # model input (or a tuple for multiple inputs)
+    "cain-temp.onnx",  # where to save the model (can be a file or file-like object)
+    export_params=True,  # store the trained parameter weights inside the model file
+    opset_version=16,  # the ONNX version to export the model to
+    do_constant_folding=True,  # whether to execute constant folding for optimization
+    input_names=input_names,  # the model's input names
+    output_names=output_names,
+    dynamic_axes={'input' : {3 : 'width', 2: 'height'}} )#
+del model
+os.system("python3 -m onnxsim cain-temp.onnx cain-sim.onnx")
+os.system(
+    f" trtexec --onnx=cain-sim.onnx --optShapes=input:1x6x{args.height}x{args.width} --fp16 --saveEngine={args.output}"
+)
