935d9a8655c9c573a1e093a300c30a94725e81dc
Hubert Sontowski <72866544+HubertSotnowski@users.noreply.github.com>
2022-11-28 23:09:48 +0100

    works but removes one output - half of speed
    * fixed dynamic shapes, improved script, removed unused files
    
    * forgot to change names
    
    * add  --trtexec param
    
    * works but i have to fix 2 outputs...

--- convert.py
+++ convert.py
@@ -4,21 +4,24 @@ import os
 from arch.vsr_arch import MSRSWVSR
 
 my_parser = argparse.ArgumentParser(description=" ")
+my_parser.add_argument("--input", metavar="--input", type=str, help="input model",default="weights/AnimeSR_v2.pth")
 my_parser.add_argument("--output", metavar="--output", type=str, help="output model")
 my_parser.add_argument("--height", metavar="--height", type=int, help="height")
 my_parser.add_argument("--width", metavar="--width", type=int, help="width")
+my_parser.add_argument("--trtexec", action="store_true", help="trtexec")
+
 args = my_parser.parse_args()
 
 
 model = MSRSWVSR(num_feat=64, num_block=[5, 3, 2], netscale=4)
 
 
-model.load_state_dict(torch.load("weights/AnimeSR_v2.pth"), strict=False)
+model.load_state_dict(torch.load(args.input), strict=False)
 input_names = ["input"]
 output_names = ["output"]
 f1 = torch.rand((1, 6, args.height, args.width))
 x = f1
-
+print(model(f1).shape)
 torch.onnx.export(
     model,  # model being run
     x,  # model input (or a tuple for multiple inputs)
@@ -30,7 +33,8 @@ torch.onnx.export(
     output_names=output_names,
     dynamic_axes={'input' : {3 : 'width', 2: 'height'}} )#
 del model
-os.system("python3 -m onnxsim animesr-temp.onnx animesr-sim.onnx")
-os.system(
-    f" trtexec --onnx=animesr-sim.onnx --optShapes=input:1x6x{args.height}x{args.width} --saveEngine={args.output}"
-)
+os.system(f"python3 -m onnxsim animesr-temp.onnx {args.output}.onnx")
+if args.trtexec:
+    os.system(
+        f" trtexec --onnx={args.output}.onnx --optShapes=input:1x6x{args.height}x{args.width} --saveEngine={args.output}.engine"
+    )
